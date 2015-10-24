9c67c69102f792d4b4327d0545fdf806e28eb3af
bartushk <kbartush7@gmail.com>
2015-10-24 15:27:06 -0400

    Initial commit.

---
+++ app/app.js
@@ -0,0 +1,3 @@
+
+console.log("Starting my app.");
+console.log("Hello world!");
---
+++ app/run_docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#docker run -it --rm --name node_app -v "$PWD":/usr/src/myapp -w /usr/src/myapp node:0.10 node app.js
+
+NODE_ARGS=""
+START_SCRIPT=""
+APP_NAME=""
+DOCKER_ARGS=""
+while [[ $# > 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -d|--debug)
+      NODE_ARGS="$NODE_ARGS debug"
+      echo "$NODE_ARGS"
+      ;;
+   -s|--script)
+     START_SCRIPT=$2
+     shift
+     ;;
+
+  esac
+shift
+done
+
+if [ "$START_SCRIPT" == "" ]
+then
+  START_SCRIPT="app.js"
+fi
+
+if [ "$APP_NAME" == "" ]
+then
+  APP_NAME="node_app"
+fi
+
+if [ "$DOCKER_ARGS" == "" ]
+then
+  DOCKER_ARGS="-it --rm"
+fi
+
+COMMAND="docker run $DOCKER_ARGS --name $APP_NAME -v $PWD:/usr/src/myapp -w /usr/src/myapp node node $NODE_ARGS $START_SCRIPT"
+echo "Starting with command: $COMMAND"
+$COMMAND
