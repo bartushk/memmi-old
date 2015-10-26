7dcfb921df5a9d1c4a57232b550c654d1e187737
bartushk <kbartush7@gmail.com>
2015-10-25 22:32:25 -0400

    Fixing wrong js operator

--- app/public/js/memmi/Card.js
+++ app/public/js/memmi/Card.js
@@ -1,8 +1,8 @@
 define(["memmi/Information"], function(information){
   function Card(cardId, frontInformation, backInformation){
-    this.cardId = cardId ? "No-Card";
-    this.frontInformation = frontInformation ? new information();
-    this.backInformation = backInformation ? new information();
+    this.cardId = cardId || "No-Card";
+    this.frontInformation = frontInformation || new information();
+    this.backInformation = backInformation || new information();
   }
 
   return Card;
