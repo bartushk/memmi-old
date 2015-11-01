9d4462429cad0e3b98b81368eccc22f790e732c4
bartushk <kbartush7@gmail.com>
2015-11-01 14:57:33 -0500

    Addes some graphics and a favicon.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+
 var app = express();
 
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
 });
