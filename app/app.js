3b4c003f15bf233fc6f3d526a46863458b37c99d
Kyle Bartush <bartushk@gmail.com>
2016-01-10 11:48:09 -0500

    Added some multi-process code

--- app/app.js
+++ app/app.js
@@ -1,29 +1,47 @@
-var express = require('express');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
+var cluster = require('cluster');
 
 
-//Setup routes.
-var pages = require('./routes/pages');
-var cardApi = require('./routes/card-api.js');
-var path = require('path');
+if(cluster.isMaster){
 
-var app = express();
+    //var cpuCount = require('os').cpus().length;
+    var cpuCount = 2;
+    console.log("CPU count: %s", cpuCount);
+    for( var i = 0; i < cpuCount; i += 1 ){
+        cluster.fork();
+    }
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    cluster.on('exit', function(worker){
+        console.log('Worker %s exited :P', worker.id);
+        cluster.fork();
+    });
+} else {
+    var express = require('express');
+    var favicon = require('serve-favicon');
+    var bodyParser = require('body-parser');
 
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', pages);
-app.use('/card-api', cardApi);
+    //Setup routes.
+    var pages = require('./routes/pages');
+    var cardApi = require('./routes/card-api.js');
+    var path = require('path');
+
+    var app = express();
+
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+
+    app.use(favicon(__dirname + '/public/images/favicon.ico'));
+    app.use(bodyParser.json());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/', pages);
+    app.use('/card-api', cardApi);
 
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+    var server = app.listen(3000, function () {
+        var host = server.address().address;
+        var port = server.address().port;
 
-    console.log('App listening at http://%s:%s', host, port);
-});
+        console.log('Worker %s listening at http://%s:%s', cluster.worker.id,  host, port);
+    });
+}
