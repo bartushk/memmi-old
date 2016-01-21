01d2e7d3a0613257d18ade6afbc4df27525451af
Kyle Bartush <bartushk@gmail.com>
2016-01-20 22:22:21 -0500

    Changing to gulp and using travis ci.

---
+++ app/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var env = require('gulp-env');
+var spawn = require('child_process').spawn; 
+var exec = require('child_process').exec; 
+var mocha = require('gulp-mocha');
+var mon = require('gulp-nodemon');
+
+gulp.task('dev', function(){
+    env({
+        vars: {
+            CONFIG: "dev"
+        }
+    });
+});
+
+
+gulp.task('dev-local', function(){
+    env({
+        vars: {
+            CONFIG: "dev-local"
+        }
+    });
+});
+
+gulp.task('test', function(){
+    gulp.src(['./test/*.js'], {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('app', function(cb){
+    mon({
+        script: './app.js',
+        ext: 'js jade json',
+        ignore: ['./node_modules'],
+        stdout: false,
+        readable: false
+        })
+    .on('change', ['lint'])
+    .on('readable', function(){
+        bunyan = spawn('./node_modules/bunyan/bin/bunyan', [
+            '--output', 'short', '--color'
+        ]);
+
+        bunyan.stdout.pipe(process.stdout);
+        bunyan.stderr.pipe(process.stderr);
+
+        this.stdout.pipe(bunyan.stdin);
+        this.stderr.pipe(bunyan.stderr);
+    });
+            
+});
+
+gulp.task('default', ['test']);
+
+gulp.task('run', ['dev-local', 'app']);
